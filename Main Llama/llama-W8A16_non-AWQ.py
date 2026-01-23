397b85dceeec8598bc460253fbf6a5cd840c1c25
phaelon74 <33295008+phaelon74@users.noreply.github.com>
2026-01-22 22:42:27 -0500

    update Act Order to Dynamic

--- Main Llama/llama-W8A16_non-AWQ.py
+++ Main Llama/llama-W8A16_non-AWQ.py
@@ -470,7 +470,7 @@ recipe = [
         ignore=["lm_head"],
         config_groups={"group_0": quant_scheme},
         dampening_frac=0.25,  # Standard GPTQ dampening (lower = more aggressive, higher = more stable)
-        actorder=True,       # Activation order optimization - improves accuracy for sensitive layers
+        actorder="dynamic",       # Activation order optimization - improves accuracy for sensitive layers
         block_size=128,      # Columns processed per pass (default: 128)
     ),
 ]
